226a9c909622d963355ad0b900a60d1e2516cbfa
xiangzhenwen <xiangzhenwen@yaochufa.com>
2019-11-07 11:52:34 +0800

    add stream test

--- src/main/java/com/imooc/web1/Web1Application.java
+++ src/main/java/com/imooc/web1/Web1Application.java
@@ -4,7 +4,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 //@ImportResource(locations = "classpath*:mapper/**.xml")
@@ -15,6 +18,12 @@ public class Web1Application {
         SpringApplication.run(Web1Application.class, args);
 
         logger.info("webstart");
+    }
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder)
+    {
+        return builder.build();
     }
 
 }
---
+++ src/test/java/com/imooc/web1/dao/RestTmpTest.java
@@ -0,0 +1,27 @@
+package com.imooc.web1.dao;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RestTmpTest {
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Test
+    public void Get()
+    {
+        URI uri = UriComponentsBuilder
+                .fromUriString("http://baidu.com/s?wd={k}")
+                .build("a");
+        String re = restTemplate.getForObject(uri, String.class);
+    }
+}
