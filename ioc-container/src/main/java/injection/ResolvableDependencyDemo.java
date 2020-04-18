6b4e75625786b0253adfc972af42e7a2e4b903f6
michael2xiang <michaelxiang@outlook.com>
2020-04-18 17:35:03 +0800

    添加外部依赖，回调拓展方式 resolvable dependency

---
+++ ioc-container/src/main/java/injection/ResolvableDependencyDemo.java
@@ -0,0 +1,35 @@
+package injection;
+
+import injection.postProcessor.CustomBeanFactoryPostProcessor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import javax.annotation.PostConstruct;
+
+public class ResolvableDependencyDemo {
+
+    @Autowired
+    private String value;
+
+    @PostConstruct
+    public void init() {
+        System.out.println(value);
+    }
+
+    public static void main(String[] args) {
+
+        AnnotationConfigApplicationContext annotationConfigApplicationContext = new AnnotationConfigApplicationContext();
+        annotationConfigApplicationContext.register(ResolvableDependencyDemo.class);
+        //回调拓展方式：参数为函数接口（实现了制定接口的class实例），加到ArrayList容器中，在后面调用
+        CustomBeanFactoryPostProcessor customBeanFactoryPostProcessor  = new CustomBeanFactoryPostProcessor();
+        annotationConfigApplicationContext.addBeanFactoryPostProcessor(customBeanFactoryPostProcessor);
+//      函数接口的写法二：lambda表达式，匿名类实例
+//        annotationConfigApplicationContext.addBeanFactoryPostProcessor(beanFactory -> {
+//            beanFactory.registerResolvableDependency(String.class, "新加外部依赖来源lamdal");
+//        });
+
+        annotationConfigApplicationContext.refresh();
+        annotationConfigApplicationContext.close();
+
+    }
+}
