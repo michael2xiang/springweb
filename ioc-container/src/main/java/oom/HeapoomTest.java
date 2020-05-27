5b13c3d075a7789d4cdcd24a3102b5410d1b2d92
michael2xiang <michaelxiang@outlook.com>
2020-05-27 23:33:36 +0800

    模拟内存泄漏

--- ioc-container/src/main/java/oom/HeapoomTest.java
+++ ioc-container/src/main/java/oom/HeapoomTest.java
@@ -1,4 +1,4 @@
- package oom;
+package oom;
 
 import com.sun.net.httpserver.HttpContext;
 import com.sun.net.httpserver.HttpExchange;
@@ -13,12 +13,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class HeapoomTest {
-    public static final int _1MB = 1024 * 1024;
+    public static final int _10MB = 1024 * 1024 * 50;
     static List<byte[]> byteList = new ArrayList<>();
-    private  static void  oom(HttpExchange exchange) throws InterruptedException {
-        try{
+
+    private static void oom(HttpExchange exchange) throws InterruptedException {
+        try {
             String response = "oom begin";
-            exchange.sendResponseHeaders(200,response.getBytes().length);
+            exchange.sendResponseHeaders(200, response.getBytes().length);
             OutputStream os = exchange.getResponseBody();
             os.write(response.getBytes());
             os.close();
@@ -27,33 +28,31 @@ public class HeapoomTest {
             e.printStackTrace();
         }
 
-        for (int i=0;;i++)
-        {
-            byte[] bytes = new byte[_1MB];
+        for (int i = 0; ; i++) {
+            byte[] bytes = new byte[_10MB];
             byteList.add(bytes);
-            System.out.printf(i+"MB");
+            System.out.printf(i + "MB");
             memPrint();
             try {
                 Thread.sleep(1000);
-            }catch (Exception ex)
-            {}
+            } catch (Exception ex) {
+            }
 
         }
 
     }
 
     private static void memPrint() {
-        for(MemoryPoolMXBean memoryPoolMXBean: ManagementFactory.getMemoryPoolMXBeans())
-        {
-            System.out.printf(memoryPoolMXBean.getName()+
-                    " commited:"+memoryPoolMXBean.getUsage().getCommitted()
-                    +" used:"+memoryPoolMXBean.getUsage().getUsed() +"\n"
-                    );
+        for (MemoryPoolMXBean memoryPoolMXBean : ManagementFactory.getMemoryPoolMXBeans()) {
+            System.out.printf(memoryPoolMXBean.getName() +
+                    " commited:" + memoryPoolMXBean.getUsage().getCommitted()
+                    + " used:" + memoryPoolMXBean.getUsage().getUsed() + "\n"
+            );
         }
     }
 
-    private  static void  srv() throws Exception {
-        HttpServer server = HttpServer.create(new InetSocketAddress(888),0);
+    private static void srv() throws Exception {
+        HttpServer server = HttpServer.create(new InetSocketAddress(888), 0);
         HttpContext context = server.createContext("/");
         context.setHandler(exchange -> {
             try {
@@ -62,11 +61,10 @@ public class HeapoomTest {
                 e.printStackTrace();
             }
         });
-        server.start();;
+        server.start();
     }
 
     public static void main(String[] args) throws Exception {
-        System.out.println("x");
         srv();
     }
 
