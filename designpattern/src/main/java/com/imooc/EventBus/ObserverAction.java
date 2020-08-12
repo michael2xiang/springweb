d81b7dfaff91dd37ed9ea6ac98f6014b55bb8aa7
xiangzhenwen <xiangzhenwen@yaochufa.com>
2020-08-12 14:20:40 +0800

    add event bus

---
+++ designpattern/src/main/java/com/imooc/EventBus/ObserverAction.java
@@ -0,0 +1,27 @@
+package com.imooc.EventBus;
+
+import org.assertj.core.util.Preconditions;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class ObserverAction {
+    private Object target;
+    private Method method;
+    public ObserverAction(Object target,Method method)
+    {
+        this.target = Preconditions.checkNotNull(target);
+        this.method = method;
+        this.method.setAccessible(true);
+    }
+    public void execute(Object param){
+        try {
+            method.invoke(target,param);
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
